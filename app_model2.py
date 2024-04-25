fdff91560b58b1438d2098ded618d9f29c4c0ab6
Alberto <albertoromv@hotmail.com>
2024-04-25 22:12:57 +0200

    WSGI añadido y ruta al fichero de la app

--- app_model2.py
+++ app_model2.py
@@ -70,8 +70,8 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 @app.route('/webhook_2024', methods=['POST'])
 def webhook():
     # Ruta al repositorio donde se realizará el pull
-    path_repo = '/ruta/a/tu/repositorio/en/PythonAnywhere'
-    servidor_web = '/ruta/al/fichero/WSGI/de/configuracion' 
+    path_repo = '/home/albertoromv/api_remota2/pythonanywhere2/'
+    servidor_web = '/var/www/albertoromv_pythonanywhere_com_wsgi.py' 
 
     # Comprueba si la solicitud POST contiene datos JSON
     if request.is_json:
